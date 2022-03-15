32c2754e29ce49615a3a791b51e41f5f65567614
qianyiloh <loh.qianyi@gmail.com>
2022-03-15 17:46:28 +0800

    Updated flow of popup windows
    Confirmation window -> Success
    Confirmation window -> Error

--- app/BooksWithdrawal.py
+++ app/BooksWithdrawal.py
@@ -2,7 +2,7 @@ import sqlalchemy as db
 from tkinter import *
 
 USERNAME = "root"
-PASSWORD = "Crunchyapples99"
+PASSWORD = "m"
 HOST = "localhost"
 PORT = 3306
 DB = "Library"
@@ -39,31 +39,14 @@ def withdraw_books():
 
     win = Tk()
 
-    # DELETE DATA FROM AUTHOR, BOOK
-
-    sql3 = "DELETE FROM Author WHERE accessionNo = '{}'".format(accessionNo)
-    cursor.execute(sql3)
-
-    sql4 = "DELETE FROM Book WHERE accessionNo = '{}'".format(accessionNo)
-    cursor.execute(sql4)
-
-    win.mainloop()
-
-def popup_window():
-
-    win = Tk()
-
-    global accessionNo
-    accessionNo = accessionNo_field.get()
-
     # Predicate: Whether book is on loan / on reservation / not in database
-    sql1 = "SELECT * FROM Borrow WHERE accessionNo = '{}'".format(accessionNo)
+    sql1 = "SELECT * FROM Borrow WHERE accessionNo = {}".format(accessionNo)
     on_loan = cursor.execute(sql1).fetchall()
-    sql2 = "SELECT * FROM Reservation WHERE accessionNo = '{}'".format(accessionNo)
+    sql2 = "SELECT * FROM Reservation WHERE accessionNo = {}".format(accessionNo)
     on_reservation = cursor.execute(sql2).fetchall()
-    sql3 = "SELECT * FROM Book WHERE accessionNo = '{}'".format(accessionNo)
+    sql3 = "SELECT * FROM Book WHERE accessionNo = {}".format(accessionNo)
     in_database = cursor.execute(sql3).fetchall()
-    
+
     if (len(on_loan) > 0):
         # Error
         label1 = Label(win, text = "Error!", font = TITLE_FONT)
@@ -90,33 +73,61 @@ def popup_window():
         btn.pack()
     else:
         # Success
-        # DELETE DATA FROM AUTHOR, BOOK
-        toplabel = Label(win, text = "Please Confirm Details to Be Correct", font = TITLE_FONT)
-        toplabel.grid(row = 1, column = 2, sticky = NSEW)
-        get_query = "SELECT * FROM Book WHERE accessionNo = '{}'".format(accessionNo)
-        get_author = "SELECT * FROM Author WHERE accessionNo = '{}'".format(accessionNo)
-        book_title = cursor.execute(get_query).fetchall()[0][1]
-        authors = cursor.execute(get_author).fetchall()[0][1]
-        isbn = cursor.execute(get_query).fetchall()[0][2]
-        publisher = cursor.execute(get_query).fetchall()[0][3]
-        year = cursor.execute(get_query).fetchall()[0][4]
+        label1 = Label(win, text = "Success!", font = TITLE_FONT)
+        label1.pack()
+        label2 = Label(win, text = "Book has been removed.", font = DEFAULT_FONT)
+        label2.pack() 
+        btn = Button(win, text = "Return to Withdrawal Function", font = DEFAULT_FONT, bg = "#5AA9E6", command = win.destroy)
+        btn.pack()
 
-        label1 = Label(win, text = "Accession Number: '{}'".format(accessionNo), font = DEFAULT_FONT, bg = "#FFE45E")
-        label1.grid(row = 2, column = 2, sticky = W)
-        label2 = Label(win, text = "Title: '{}'".format(book_title), font = DEFAULT_FONT, bg = "#FFE45E")
-        label2.grid(row = 3, column = 2, sticky = W)
-        label3 = Label(win, text = "Authors: '{}'".format(authors), font = DEFAULT_FONT, bg = "#FFE45E")
-        label3.grid(row = 4, column = 2, sticky = W)
-        label4 = Label(win, text = "ISBN: '{}'".format(isbn), font = DEFAULT_FONT, bg = "#FFE45E")
-        label4.grid(row = 5, column = 2, sticky = W)
-        label5 = Label(win, text = "Publisher: '{}'".format(publisher), font = DEFAULT_FONT, bg = "#FFE45E")
-        label5.grid(row = 6, column = 2, sticky = W)
-        label6 = Label(win, text = "Year: '{}'".format(year), font = DEFAULT_FONT, bg = "#FFE45E")
-        label6.grid(row = 7, column = 2, sticky = W)
+        # DELETE DATA FROM AUTHOR
+        sql3 = "DELETE FROM Author WHERE accessionNo = {}".format(accessionNo)
+        cursor.execute(sql3)
 
-        button1 = Button(win, text = "Confirm Withdrawal", font = DEFAULT_FONT, bg = "#5AA9E6", command = lambda: [withdraw_books(), win.destroy()])
-        button1.grid(row = 8, column = 2)
-        button2 = Button(win, text = "Back to Withdrawal Function", font = DEFAULT_FONT, bg = "#5AA9E6", command = win.destroy)
-        button2.grid(row = 8, column = 3)
+        # DELETE DATA FROM BOOK
+        sql4 = "DELETE FROM Book WHERE accessionNo = {}".format(accessionNo)
+        cursor.execute(sql4)
+
+    win.mainloop()
+
+def popup_window():
+
+    win = Tk()
+
+    global accessionNo
+    accessionNo = accessionNo_field.get()
+
+    # Green confirmation window
+    toplabel = Label(win, text = "Please Confirm Details to Be Correct", font = TITLE_FONT)
+    toplabel.grid(row = 1, column = 2, sticky = NSEW)
+    get_query = "SELECT * FROM Book WHERE accessionNo = {}".format(accessionNo)
+    get_author = "SELECT * FROM Author WHERE accessionNo = {}".format(accessionNo)
+    book_title = cursor.execute(get_query).fetchall()[0][1]
+    author = cursor.execute(get_author).fetchall()
+    isbn = cursor.execute(get_query).fetchall()[0][2]
+    publisher = cursor.execute(get_query).fetchall()[0][3]
+    year = cursor.execute(get_query).fetchall()[0][4]
+
+    authors = []
+    for i in range(len(author)):
+        authors = authors.append(cursor.execute(get_author).fetchall()[i][1])
+        
+    label1 = Label(win, text = "Accession Number: {}".format(accessionNo), font = DEFAULT_FONT, bg = "#FFE45E")
+    label1.grid(row = 2, column = 2, sticky = W)
+    label2 = Label(win, text = "Title: {}".format(book_title), font = DEFAULT_FONT, bg = "#FFE45E")
+    label2.grid(row = 3, column = 2, sticky = W)
+    label3 = Label(win, text = "Authors: {}".format(', '.join(authors)), font = DEFAULT_FONT, bg = "#FFE45E")
+    label3.grid(row = 4, column = 2, sticky = W)
+    label4 = Label(win, text = "ISBN: {}".format(isbn), font = DEFAULT_FONT, bg = "#FFE45E")
+    label4.grid(row = 5, column = 2, sticky = W)
+    label5 = Label(win, text = "Publisher: {}".format(publisher), font = DEFAULT_FONT, bg = "#FFE45E")
+    label5.grid(row = 6, column = 2, sticky = W)
+    label6 = Label(win, text = "Year: {}".format(year), font = DEFAULT_FONT, bg = "#FFE45E")
+    label6.grid(row = 7, column = 2, sticky = W)
+
+    button1 = Button(win, text = "Confirm Withdrawal", font = DEFAULT_FONT, bg = "#5AA9E6", command = lambda: [withdraw_books(), win.destroy()])
+    button1.grid(row = 8, column = 2)
+    button2 = Button(win, text = "Back to Withdrawal Function", font = DEFAULT_FONT, bg = "#5AA9E6", command = win.destroy)
+    button2.grid(row = 8, column = 3)
 
     win.mainloop()
