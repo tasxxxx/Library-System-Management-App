8beea6cc4c91827645c119604fac7850238a58e8
Brexton Ho <e0564446@u.nus.edu>
2022-03-16 21:54:00 +0800

    design for fine
    fine_main, pay_fine

--- app/fine_main.py
+++ app/fine_main.py
@@ -2,15 +2,24 @@ from tkinter import *
 from PIL import Image, ImageTk
 from pay_fine import fine_details
 
+FONT = 'Arial'
+FONT_SIZE = 25
+SMALL_FONT_SIZE = 10
+STYLE = 'bold'
+
 #slide 41
 def fine_main_menu():
     win = Toplevel()
     win.title("Fine")
-    win.geometry("600x300")
+    win.geometry("1920x1080")
 
-    title_frame = LabelFrame(win, padx = 5, pady = 5)
-    title_frame.grid(row = 0, column = 0, columnspan = 1, padx = 10, pady = 10)
-    title_label = Label(title_frame, text = "Select one of the options below").pack()
+    # title_frame = LabelFrame(win, padx = 5, pady = 5)
+    # title_frame.grid(row = 0, column = 0, columnspan = 1, padx = 10, pady = 10)
+    # title_label = Label(title_frame, text = "Select one of the options below").pack()
+
+    instructions = Label(win, text='Select one of the Options below:', fg='black', bg='#c5e3e5', relief='raised', width=60, height=3)
+    instructions.config(font=(FONT, FONT_SIZE, STYLE))
+    instructions.place(relx=0.5, rely=0.09, anchor="center")
 
     # Creating Image
     img = Image.open("fines_photo.jpg")
@@ -19,14 +28,21 @@ def fine_main_menu():
 
     # Putting Image on the window instead of menu page
     img_canvas = Canvas(win, width=400, height=300)
-    img_canvas.grid(row=1, column=0, rowspan=5, padx=10, pady=10)
+    img_canvas.place(relx=0.2, rely=0.50, anchor="w")
     img_canvas.create_image(10, 10, anchor="nw", image=my_img)
 
     #creating the buttons
-    reserve_button = Button(win, text = "Fine Payment", command = fine_details) #command for fine payment
-    reserve_button.grid(row = 1, column = 3)
+    reserve_button = Button(win, text = "10. Payment", command = fine_details) #command for fine payment
+    reserve_button.config(font=(FONT, FONT_SIZE, STYLE))
+    reserve_button.place(relx=0.45, rely=0.45, anchor="center")
 
-    back_button = Button(win, text = "Back to Main Menu", command = win.destroy)
-    back_button.grid(row = 1, column = 4)
+    desc = Label(win, text='Fine Payment', fg='black')
+    desc.config(font=(FONT, FONT_SIZE, STYLE))
+    desc.place(relx=0.65, rely=0.45, anchor="center")
+
+    btn_home = Button(win, text='Back to Main Menu', bg='#c5e3e5', relief='raised', width=60, height=1, command = win.destroy)
+    btn_home.config(font=(FONT, FONT_SIZE, STYLE))
+    btn_home.place(relx=0.5, rely=0.80, anchor="center")
 
     win.mainloop()
+
