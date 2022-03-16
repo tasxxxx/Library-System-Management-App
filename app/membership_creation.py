465c9a669bdf8d5adb5c0d8de30c232fca14ab4f
Brexton Ho <e0564446@u.nus.edu>
2022-03-16 23:03:10 +0800

    design for Member
    membership_creation, membership_deletion, membership_update

--- app/membership_creation.py
+++ app/membership_creation.py
@@ -9,7 +9,7 @@ SMALL_FONT_SIZE = 10
 STYLE = 'bold'
 
 USERNAME = "root"
-PASSWORD = "" ## insert password
+PASSWORD = "" ## enter password
 HOST = "localhost"
 PORT = 3306
 DB = "Library"
@@ -21,7 +21,7 @@ cursor = engine.connect()
 def createMember():
 
 	win = tk.Tk()
-	win.geometry("1920x1080")
+	win.geometry("800x400")
 
 	global ent_memId
 	global ent_name
@@ -44,12 +44,19 @@ def createMember():
 	## slide 7
 	# check if fields are missing or incomplete, or member already exists
 	if memId == "" or name == "" or faculty == "" or phoneNo == "" or email == "" or valid_membership_id:
-		label1 = tk.Label(win, text="Error!", bg="#cc0505", fg= "#ffff00")
-		label1.pack()
-		label2 = tk.Label(win, text="Member already exists; Missing or Incomplete fields.")
-		label2.pack()
+		win.configure(bg = "#eb1e1e")
+
+		label1 = tk.Label(win, text="Error!", bg = "#eb1e1e", fg= "#ffff00")
+		label1.config(font=(FONT, FONT_SIZE, STYLE))
+		label1.place(relx=0.5, rely=0.15, anchor="center")
+
+		label2 = tk.Label(win, text="Member already exists; Missing or Incomplete fields.", bg = "#eb1e1e")
+		label2.config(font=(FONT, 20, STYLE))
+		label2.place(relx=0.5, rely=0.3, anchor="center")
+
 		btn = tk.Button(win, text="Back to Create Function", command=win.destroy)
-		btn.pack()
+		btn.config(font=(FONT, 20, STYLE))
+		btn.place(relx=0.5, rely=0.8, anchor='center')
 
 		win.mainloop()
 
@@ -57,25 +64,39 @@ def createMember():
 	else:
 
 		try:
+			win.configure(bg = "#b0f556")
+
 			insert_member = "INSERT INTO Members VALUES ('{}', '{}', '{}', '{}', '{}')".format(memId, name, faculty, phoneNo, email)
 			cursor.execute(insert_member)
 
-			label1 = tk.Label(win, text="Success!")
-			label1.pack()
-			label2 = tk.Label(win, text="ALS Membership created.")
-			label2.pack()
+			label1 = tk.Label(win, text="Success!", bg = "#b0f556")
+			label1.config(font=(FONT, FONT_SIZE, STYLE))
+			label1.place(relx=0.5, rely=0.15, anchor="center")
+
+			label2 = tk.Label(win, text="ALS Membership created.", bg = "#b0f556")
+			label2.config(font=(FONT, FONT_SIZE, STYLE))
+			label2.place(relx=0.5, rely=0.3, anchor="center")
+
 			btn = tk.Button(win, text="Back to Create Function", command=win.destroy)
-			btn.pack()
+			btn.config(font=(FONT, 20, STYLE))
+			btn.place(relx=0.5, rely=0.8, anchor='center')
 
 			win.mainloop()
 
 		except (IntegrityError, ValueError, OperationalError):
+			win.configure(bg = "#eb1e1e")
+
 			label1 = tk.Label(win, text="Error!", bg="#cc0505", fg= "#ffff00")
-			label1.pack()
+			label1.config(font=(FONT, FONT_SIZE, STYLE))
+			label1.place(relx=0.5, rely=0.15, anchor="center")
+
 			label2 = tk.Label(win, text="Invalid entry")
-			label2.pack()
+			label2.config(font=(FONT, FONT_SIZE, STYLE))
+			label2.place(relx=0.5, rely=0.3, anchor="center")
+
 			btn = tk.Button(win, text="Back to Create Function", command=win.destroy)
-			btn.pack()
+			btn.config(font=(FONT, 20, STYLE))
+			btn.place(relx=0.5, rely=0.8, anchor='center')
 
 			win.mainloop()
 
