e7a9dc9f27091b5f69d8d7038ad0ff4b388d8d3c
a.anghs22 <a.anghs22@gmail.com>
2022-03-16 23:30:08 +0800

    standardised gui

--- app/reservation_main.py
+++ app/reservation_main.py
@@ -3,6 +3,10 @@ from PIL import Image, ImageTk
 from make_reservation import reservation_details
 from cancel_reservation import cancel_reservation_details
 
+FONT = 'Arial'
+FONT_SIZE = 25
+SMALL_FONT_SIZE = 10
+STYLE = 'bold'
 
 #slide 33
 def reservation_main_menu():
@@ -18,22 +22,30 @@ def reservation_main_menu():
 	canvas1.pack(fill = "both", expand =  True)
 	canvas1.create_image(0, 0, image = bg, anchor = "nw")
 
-    title_frame = LabelFrame(win, padx = 5, pady = 5)
-    title_frame.grid(row = 0, column = 0, columnspan = 1, padx = 10, pady = 10)
-    title_label = Label(title_frame, text = "Select one of the options below").pack()
+    label = tk.Label(root, text="Select one of the Options below", fg='black', bg='#c5e3e5', relief='raised', width=60,
+                     height=3)
+    label.config(font=(FONT, FONT_SIZE, STYLE))
+    label.place(relx=0.5, rely=0.09, anchor="center")
 
-    my_img = ImageTk.PhotoImage(Image.open("reservations_photo.jpg").resize((200, 200), Image.ANTIALIAS))
-    my_label = Label(image = my_img)
-    my_label.grid(row = 1, column = 0)
+    image = Image.open("reservations_photo.jpg")
+    image = image.resize((400, 300), Image.ANTIALIAS)
+    my_img = ImageTk.PhotoImage(image)
+    img_canvas = tk.Canvas(root, width=300, height=250)
+    img_canvas.place(relx=0.2, rely=0.50, anchor="w")
+    img_canvas.create_image(50, 50, anchor="w", image=my_img)
+
 
     #creating the buttons
     reserve_button = Button(win, text = "Book Reservation", command = reservation_details)
-    reserve_button.grid(row = 1, column = 3)
+	btn_create.config(font=(FONT, FONT_SIZE, STYLE))
+	btn_create.place(relx=0.6, rely=0.3, anchor="center")
 
     cancel_button = Button(win, text = "Cancel Reservation", command = cancel_reservation_details)
-    cancel_button.grid(row = 1, column = 4)
+    cancel_button.config(font=(FONT, FONT_SIZE, STYLE))
+	cancel_button.place(relx=0.6, rely=0.45, anchor="center")
 
     back_button = Button(win, text = "Back to Main Menu", command = win.destroy)
-    back_button.grid(row = 1, column = 5)
-
+    back_button.config(font=(FONT, FONT_SIZE, STYLE))
+	back_button.place(relx=0.5, rely=0.80, anchor="center")
+    
     win.mainloop()
